79d229da33c9edb2756c946c0460117c0e8c2473
Powerleech <59046583+Powerleech@users.noreply.github.com>
2022-03-28 16:00:17 +0200

    Update dockerdeploy.sh

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -2,21 +2,24 @@
 
 repos=('api' 'test-api' 'data-api' 'realtime_websocket' 'banking-app')
 REPO_NAME=$1
+EXTERNAL_PORT=$2
+VERSION=${3:-latest}
+
+
 
 Help()
 {
   # Display Help
   echo "
    *****************************************************************************************************************
-   *      Run the script with exactly one argument - the name of the docker repo!                                  *
+   *      Run the script with the following args                                                                   *
+   *        - 1) the name of the repo you are trying top deploy (e.g. test-api)                                    *
+   *        - 2) A valid external port for the container (e.g. 8542)                                               *
+   *        - (3) - OPTIONAL - A version tag of the image. If none given, it will be latest                        *
    *                                                                                                               *
    *                                                                                                               *
    *                                                                                                               *
-   *      The name you should give is dockerhub user followed by name of the repo                                  *
-   *      FIY: Currently this script does not take tags and versioning into account.                               *
-   *      thus, this script will stop ALL containers within the repo name, pull the latest version and run that.   *
-   *                                                                                                               *
-   *      e.g. bash dockerdeploy.sh dockerhubusername/reponame                                                     *
+   *      e.g. sh dockerdeploy.sh test-api 8542 1.0.5                                                              *
    *                                                                                                               *
    *      chose among the following repos:                                                                         *
    *        - api                                                                                                  *
@@ -30,6 +33,49 @@ Help()
  "
 }
 
+is_valid_repo() {
+    for repo in "${repos[@]}"
+    do
+        [[ "$repo" == "$REPO_NAME" ]] && return
+    done
+
+   echo "not a valid repo"
+   exit
+}
+
+create_temp_env() {
+    myFileName="env-${REPO_NAME}"
+    if test -e envfiles/."${myFileName}"; then
+        deployName="${myFileName}-deploy"
+        cp envfiles/."${myFileName}" envfiles/."${deployName}"
+        # delete empty lines
+        sed -i '/^$/d' envfiles/."$deployName"
+        # delete lines without '='
+        sed -n -i '/=/p' envfiles/."$deployName"
+        # trim '='
+        sed -r -i 's/\s*(=)\s*/\1/g' envfiles/."$deployName"
+        echo "Env file created for production: .${deployName}"
+    else
+        echo "no env file was found with name ${myFileName}. The container won't run with a env file."
+    fi
+}
+
+
+refresh_docker_container(){
+FULL_REPO_NAME="raskraskdk/$REPO_NAME"
+    if [[ $(docker ps -q --filter ancestor=$FULL_REPO_NAME ) ]]; then
+        docker stop $(docker ps -q --filter ancestor=$FULL_REPO_NAME )
+        docker rm $(docker ps -a -q --filter ancestor=$FULL_REPO_NAME )
+    fi
+
+    [[ $(docker images -q $FULL_REPO_NAME:$VERSION) ]] && docker rmi -f $(docker images -q $FULL_REPO_NAME )
+    docker pull $FULL_REPO_NAME:$VERSION
+    docker run --env-file envfiles/."${deployName}" -p $EXTERNAL_PORT:3000 --name $REPO_NAME -d $FULL_REPO_NAME:$VERSION
+}
+
+# ******************************************** Run script ****************************************
+
+# check options given
 while getopts ":h" option; do
   case $option in
     h) # display Help
@@ -41,43 +87,32 @@ while getopts ":h" option; do
   esac
 done
 
-if [[ $# -eq 0 ]]; then
+# Check if reponame is given
+if [[ -z "$1" ]]; then
   Help
-  echo "No arguments provided"
+  echo "A reponame is required"
   exit 1
 fi
 
-is_valid_repo() {
-    for repo in "${repos[@]}"
-    do
-        [[ "$repo" == "$REPO_NAME" ]] && return
-    done
-
-   echo "no a valid repo"
-   exit
-}
-
-create_temp_env() {
-    myFileName="env-${REPO_NAME}"
-    deployName="${myFileName}-deploy"
-    echo $myFileName
-    echo $deployName
-    cp ./."${myFileName}" ./."${deployName}"
-    sed -i '/^$/d' ./."$deployName"
-    sed -n -i '/=/p' ./."$deployName"
-    sed -r -i 's/\s*(=)\s*/\1/g' ./."$deployName"
-    echo "Env file created for production: .${deployName}"
-}
-
-
-refresh_docker_container(){
-FULL_REPO_NAME="raskraskdk/$REPO_NAME"
-EXISTING_ID=$(docker ps -q --filter ancestor=$FULL_REPO_NAME)
-
-}
-
-
-
+# Check if the reponame is valid
 is_valid_repo
+
+# Check if external port is given
+if [ -z "$2" ] || ! [[ "$2" =~ [0-9]{4,} ]]; then
+    Help
+    echo "A valid external port must be specified"
+    exit 1
+fi
+
+# Check if the optional version number is valid
+if ! [ -z "$3" ] && ! [[ "$3" =~ v[0-9]+\.[0-9]+\.[0-9]+  ]]; then
+    Help
+    echo "Please provide a valid version number"
+    exit 1
+fi
+
+# Create the production env file ready
 create_temp_env
 refresh_docker_container
+docker ps -f name=$REPO_NAME
+docker logs $REPO_NAME
