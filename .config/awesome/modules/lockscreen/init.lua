a2eb07591655a85561c245c12ab9a6efc8b33b14
powerleech <moneyhonster@gmail.com>
2022-08-09 23:28:31 +0200

    new theme

---
+++ .config/awesome/modules/lockscreen/init.lua
@@ -0,0 +1,7 @@
+local awful = require("awful")
+
+local function lock_screen_show()
+  awful.spawn.with_shell("sh i3lock-extra.sh -i $HOME/.active-wallpaper/wallpaper.jpg -o $HOME/Pictures/lock-overlay.png")
+end
+
+return lock_screen_show
